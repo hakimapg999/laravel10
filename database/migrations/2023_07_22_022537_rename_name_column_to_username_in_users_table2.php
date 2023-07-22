1ec09a7b79f478d18af04bf1d2bdb39dd48c45a2
M Nisfan Hakim Ginting <56389771+einhazard@users.noreply.github.com>
2023-07-22 09:31:47 +0700

    test

---
+++ database/migrations/2023_07_22_022537_rename_name_column_to_username_in_users_table2.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('id', 'IDUser');
+            $table->renameColumn('name', 'username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('IDUser', 'id');
+            $table->renameColumn('username', 'name');
+        });
+    }
+};
